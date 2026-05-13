b61c12b5eed8f83eccff88280bb6822cb9127951
Toby Xuan Thanh <xuanthanh88508@gmail.com>
2026-05-13 18:59:01 +0700

    Init FE

---
+++ fe/src/lib/axios.ts
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access_token");
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+});
+
+api.interceptors.response.use(
+  (res) => res,
+  async (err) => {
+    if (err.response?.status === 401) {
+      // Gọi /auth/refresh rồi retry
+    }
+    return Promise.reject(err);
+  },
+);
+
+export default api;
